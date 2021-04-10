8497c0b6d57fc57e8735c61cce8ca196dd0e80ab
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2021-04-10 10:18:28 -0400

    bat

---
+++ git-init.bat
@@ -0,0 +1,12 @@
+mkdir %1
+cd %1
+type nul>README.md 
+
+git init
+git add .
+git commit -m "init commit"
+git branch -M main
+git remote add origin https://github.com/aidanjrauscher/%1.git
+git push -u origin main
+
+ECHO Succcessfully initialized %1 repository!

c94954894da416f276cc4d733fdbc4550f8b58d1
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2021-04-11 20:31:11 -0400

    Update git-init.bat

--- git-init.bat
+++ git-init.bat
@@ -6,7 +6,7 @@ git init
 git add .
 git commit -m "init commit"
 git branch -M main
-git remote add origin https://github.com/%2/%1.git
+git remote add origin https://github.com/[github-username]/%1.git
 git push -u origin main
 
 ECHO Succcessfully initialized %1 repository for %2!
